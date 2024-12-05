99f3042cfd040f2837c43aa034bc22177b846921
Sophie <sophiesokol123@gmail.com>
2024-12-05 20:19:32 +0000

    Create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from './components/Header'
 
 function App() {
   return (
-    <>
-    </>
+    <main className='main justify-center lg:justify-start'>
+      <Header />
+      <div className='content'>
+
+      </div>
+      </main>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { IoMoonOutline } from "react-icons/io5";
+
+export default function Header() {
+    return (
+        <div className="header flex items-center justify-between p-4">
+            <span className="pl-8 text-xl font-bold">Where in the world?</span>
+            <button className="pr-8 text-sm flex justify-center items-center"><IoMoonOutline className="mr-2"/> Dark Mode</button>
+        </div>
+    )
+}
